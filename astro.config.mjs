f9b76af2c7a94e55c2d1bbb55cda6876f5248872
Nnawir <riwann4.guyader@gmail.com>
2026-09-22 20:58:12 +0200

    Update Site source

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ninja-tcg-resource.chessnnawir.chatgpt.site',
+  site: 'https://narutocardguide-resource.chessnnawir.chatgpt.site',
   trailingSlash: 'always',
   redirects: {
     '/actualites/': '/news/',
